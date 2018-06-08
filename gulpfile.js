bc48cd0d8151be73947e7829b8782baa4a09db83
Anthony Mandra <anthony.mandra@keysight.com>
2018-06-08 15:59:16 -0400

    readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,12 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-var autoprefixer = require("gulp-autoprefixer");
-// var log = require('fancy-log')
 
 gulp.task('sass', function () {
     return gulp.src('*.scss')
       .pipe(sass().on('error', sass.logError))
-    //   .pipe(autoprefixer("last 20 version", "safari 5", "ie 8", "ie 9"))
       .pipe(gulp.dest('.'));
 });
 
 gulp.task('watch', function () {
     gulp.watch('*.scss', ['sass']);
 });
-
-// function sass() {
-//     // log("Generating css");
-  
-//     gulp.src('./*.scss')
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
-//         // .pipe(concat('resume.css'))
-//         .pipe(gulp.dest("./css"))
-// }
-
-// function watch() {
-//     // log("Watching scss files for modifications");
-//     gulp.watch('*.scss', sass);
-//   }
-
-// exports.sass = sass;
-// exports.watch = watch;
-
-// gulp.task("build", [sass]);
-// gulp.task('default', build);
-
