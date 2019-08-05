336e65d01d87b26281dede6f1a862b15b9fe6952
Anthony Mandra <amandra@gmail.com>
2019-08-05 12:47:01 -0400

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,48 +1,6 @@
 <?php
 // Forked from https://gist.github.com/1809044
 // Available from https://gist.github.com/nichtich/5290675#file-deploy-php
-$TITLE   = 'Git Deployment Hamster';
-$VERSION = '0.11';
-echo <<<EOT
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>$TITLE</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
-  o-o    $TITLE
- /\\"/\   v$VERSION
-(`=*=') 
- ^---^`-.
-EOT;
-// Check whether client is allowed to trigger an update
-$allowed_ips = array(
-	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', // GitHub
-	'195.37.139.','193.174.' // VZG
-);
-$allowed = false;
-$headers = apache_request_headers();
-if (@$headers["X-Forwarded-For"]) {
-    $ips = explode(",",$headers["X-Forwarded-For"]);
-    $ip  = $ips[0];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-foreach ($allowed_ips as $allow) {
-    if (stripos($ip, $allow) !== false) {
-        $allowed = true;
-        break;
-    }
-}
-if (!$allowed) {
-	header('HTTP/1.1 403 Forbidden');
- 	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
-    echo "</pre>\n</body>\n</html>";
-    exit;
-}
-flush();
 
 require_once '/var/secure/convertapi.php';
 $convertUrl = "https://v2.convertapi.com/web/to/pdf?Secret=$convertapiKey&download=inline&url=http://resume.anthonymandra.com";
@@ -70,6 +28,3 @@ $log .= "\n";
 file_put_contents ('deploy-log.txt',$log,FILE_APPEND);
 echo $output; 
 ?>
-</pre>
-</body>
-</html>
