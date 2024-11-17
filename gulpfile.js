0bdf0a8e285019301235ccd32af38e12ff934dc9
Anthony Mandra <amandra@gmail.com>
2024-11-17 12:34:08 -0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
+const gulp = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('sass', function () {
     return gulp.src('*.scss')
